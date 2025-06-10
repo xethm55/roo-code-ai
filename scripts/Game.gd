ec00ecba3bcdeb9c196518d2ea4f35f20cb336bf
xethm55 <xethm55@yahoo.com>
2025-06-09 23:19:34 -0400

    Starting to add local server support for testing

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 func _ready():
-    # Instantiate players based on network data
-    NetworkManager.connect("player_joined", self, "_on_player_joined")
-    
-    # Show color selection UI for local player
-    $UI/ColorSelection.visible = true
+	# Instantiate players based on network data
+	NetworkManager.connect("player_joined", self, "_on_player_joined")
+	
+	# Show color selection UI for local player
+	$UI/ColorSelection.visible = true
 
 func _on_player_joined(player_id):
-    var player_scene = preload("res://scenes/Player.tscn")
-    var player = player_scene.instance()
-    player.player_id = player_id
-    $Players.add_child(player)
+	var player_scene = preload("res://scenes/Player.tscn")
+	var player = player_scene.instance()
+	player.player_id = player_id
+	$Players.add_child(player)
--- scripts/NetworkManager.gd
+++ scripts/NetworkManager.gd
@@ -14,29 +14,38 @@ signal connection_established()
 
 func authenticate(nickname: String):
 	player_nickname = nickname
-	# Using the latest Hathora authentication method
-	var res = await HathoraSDK.auth_v1.login_nickname(player_nickname).async()
 	
-	if res.is_error():
-		print("Authentication failed: ", res.as_error().message)
-		emit_signal("authentication_result", false)
-		return
+	# For local development, skip remote authentication
+	if OS.has_environment("HATHORA_LOCAL"):
+		auth_token = "local_token" 
+		print("Local authentication for: ", player_nickname)
+		emit_signal("authentication_result", true)
+		# Join a hardcoded local room
+		join_room("local-room", player_color)
+	else:
+		# Remote authentication
+		var res = await HathoraSDK.auth_v1.login_nickname(player_nickname).async()
 		
-	auth_token = res.get_data().token
-	print("Authenticated as: ", player_nickname)
-	emit_signal("authentication_result", true)
-	
-	# After authentication, get lobby info and join room
-	var lobby_res = await HathoraSDK.lobby_v3.get_info_by_short_code(
-		auth_token,
-		"default"  # Use your lobby short code here
-	).async()
-	
-	if lobby_res.is_error():
-		print("Lobby info error: ", lobby_res.as_error().message)
-		return
+		if res.is_error():
+			print("Authentication failed: ", res.as_error().message)
+			emit_signal("authentication_result", false)
+			return
+			
+		auth_token = res.get_data().token
+		print("Authenticated as: ", player_nickname)
+		emit_signal("authentication_result", true)
 		
-	join_room(lobby_res.get_data().roomId, player_color)
+		# After authentication, get lobby info and join room
+		var lobby_res = await HathoraSDK.lobby_v3.get_info_by_short_code(
+			auth_token,
+			"default"  # Use your lobby short code here
+		).async()
+		
+		if lobby_res.is_error():
+			print("Lobby info error: ", lobby_res.as_error().message)
+			return
+			
+		join_room(lobby_res.get_data().roomId, player_color)
 
 func create_room(game_type: String, color: Color):
 	player_color = color
@@ -76,10 +85,15 @@ func join_room_id(room_id: String):
 		
 	# Create multiplayer client
 	var peer = ENetMultiplayerPeer.new()
-	var err = peer.create_client(
-		connection_info.exposedPort.host,
-		connection_info.exposedPort.port
-	)
+	var host = connection_info.exposedPort.host
+	var port = connection_info.exposedPort.port
+	
+	# For local development, connect directly to localhost
+	if OS.has_environment("HATHORA_LOCAL"):
+		host = "localhost"
+		print("Using local Hathora server")
+	
+	var err = peer.create_client(host, port)
 	
 	if err:
 		print("Connection failed: ", err)
@@ -87,7 +101,7 @@ func join_room_id(room_id: String):
 		
 	multiplayer.multiplayer_peer = peer
 	await multiplayer.connected_to_server
-	print("Connected to Hathora server!")
+	print("Connected to Hathora server at %s:%d!" % [host, port])
 	emit_signal("connection_established")
 	get_tree().change_scene("res://scenes/GameScene.tscn")
 
